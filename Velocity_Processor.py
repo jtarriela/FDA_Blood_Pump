6e937b3b0f930d32bccfe8058ebef8b1cfa164a8
jtarriela <78770359+jtarriela@users.noreply.github.com>
2021-02-16 18:59:52 -0500

    added radius calculation to dataframe

--- Velocity_Processor.py
+++ Velocity_Processor.py
@@ -4,4 +4,7 @@ import numpy as np
 """The intention of this script is to create classes to be called" \
 "to compute velocity plots for different test conditions"""
 
-df_1 = pd.read_csv(r'C:\Users\Joseph Tarriela\Documents\GitHub\FDA_Blood_Pump\Velocity_Data\K-omega_6L_2500\VelocityMagnitude-Q-1-k-omg.csv')
+url_df_1=r'C:\Users\Joseph Tarriela\Documents\GitHub\FDA_Blood_Pump\Velocity_Data\K-omega_6L_2500\MeanVelocityMagnitude-Q-1-k-omg.csv'
+df_1 = pd.read_csv(url_df_1, index_col=0)
+df_1.insert(3,"radius",0)
+df_1['radius']=np.sqrt(df_1['    x-coordinate']**2+df_1['    y-coordinate']**2)
