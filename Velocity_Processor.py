59c8966c49e35b023cd24dddf71cb206321b8d41
jtarriela <78770359+jtarriela@users.noreply.github.com>
2021-02-16 18:31:17 -0500

    Initial commit

--- Velocity_Processor.py
+++ Velocity_Processor.py
@@ -1,4 +1,7 @@
 import pandas as pd
 import numpy as np
 
-print("This script processes and plots Fluent magnitude data.")
+"""The intention of this script is to create classes to be called" \
+"to compute velocity plots for different test conditions"""
+
+df_1 = pd.read_csv(r'C:\Users\Joseph Tarriela\Documents\GitHub\FDA_Blood_Pump\Velocity_Data\K-omega_6L_2500\VelocityMagnitude-Q-1-k-omg.csv')
