723dd375c9f01b1925e025e868c51dbf0270402b
jtarriela <78770359+jtarriela@users.noreply.github.com>
2021-02-16 19:51:54 -0500

    added radius calculation to dataframe

--- Velocity_Processor.py
+++ Velocity_Processor.py
@@ -6,7 +6,15 @@ import matplotlib.pyplot as plt
 ### The intention of this script is to create classes to be called
 ### to compute velocity plots for different test conditions
 
+
+ ## number of simultaneous csv's to process
+
+#Source URL here
 url_df_1=r'C:\Users\Joseph Tarriela\Documents\GitHub\FDA_Blood_Pump\Velocity_Data\K-omega_6L_2500\VelocityMagnitude-Q-1-k-omg.csv'
+url_df_2=r'C:\Users\Joseph Tarriela\Documents\GitHub\FDA_Blood_Pump\Velocity_Data\K-omega_6L_2500\RMSEVelocityMagnitude-Q-1-k-omg.csv'
+url_df_3=r'C:\Users\Joseph Tarriela\Documents\GitHub\FDA_Blood_Pump\Velocity_Data\K-omega_6L_2500\MeanVelocityMagnitude-Q-1-k-omg.csv'
+
+files_to_process=[url_df_1,url_df_2,url_df_3] # modify array with correct vars
 
 df_1 = pd.read_csv(url_df_1, index_col=0)
 df_1.insert(3,"radius",0)
@@ -18,4 +26,3 @@ df_1.drop(indexNames , inplace=True)
 
 df_1.plot(kind='scatter',x='radius',y=df_1_header_list[4],color='red')
 plt.show()
-
