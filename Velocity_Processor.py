3c9258edfe94076aef7889e60516bf390fbf0d64
jtarriela <78770359+jtarriela@users.noreply.github.com>
2021-02-16 21:12:43 -0500

    added radius calculation to dataframe

--- Velocity_Processor.py
+++ Velocity_Processor.py
@@ -14,15 +14,47 @@ url_df_1=r'C:\Users\Joseph Tarriela\Documents\GitHub\FDA_Blood_Pump\Velocity_Dat
 url_df_2=r'C:\Users\Joseph Tarriela\Documents\GitHub\FDA_Blood_Pump\Velocity_Data\K-omega_6L_2500\RMSEVelocityMagnitude-Q-1-k-omg.csv'
 url_df_3=r'C:\Users\Joseph Tarriela\Documents\GitHub\FDA_Blood_Pump\Velocity_Data\K-omega_6L_2500\MeanVelocityMagnitude-Q-1-k-omg.csv'
 
-files_to_process=[url_df_1,url_df_2,url_df_3] # modify array with correct vars
+# files_to_process=[url_df_1,url_df_2,url_df_3] # modify array with correct vars
 
-df_1 = pd.read_csv(url_df_1, index_col=0)
-df_1.insert(3,"radius",0)
-df_1['radius']=np.sqrt(df_1['    x-coordinate']**2+df_1['    y-coordinate']**2)
-df_1_header_list=list(df_1.columns.values)
+# class df_cleaner():
+#     def __init__(self, url):
+#         self.url = url
+#
+#     def compute_radius(self):
+#         df = pd.read_csv(self.url, index_col=0)
+#         df.insert(3, "radius", 0)
+#         df['radius'] = np.sqrt(df['    x-coordinate'] ** 2 + df['    y-coordinate'] ** 2)
+#         indexNames = df[(df['radius'] > 0.03)].index
+#         df.drop(indexNames, inplace=True)
+#         return df
 
-indexNames = df_1[ (df_1['radius'] > 0.03)].index
-df_1.drop(indexNames , inplace=True)
+class df_cleaner():
+    def __init__(self, url):
+        self.url = url
 
-df_1.plot(kind='scatter',x='radius',y=df_1_header_list[4],color='red')
-plt.show()
+    @classmethod
+    def compute_radius(cls, self):
+        df = pd.read_csv(self.url, index_col=0)
+        df.insert(3, "radius", 0)
+        df['radius'] = np.sqrt(df['    x-coordinate'] ** 2 + df['    y-coordinate'] ** 2)
+        indexNames = df[(df['radius'] > 0.03)].index
+        df.drop(indexNames, inplace=True)
+        self.df=df
+
+rad = df_cleaner(url_df_1)
+df_cleaner.compute_radius(rad)
+
+
+# def compute_radius(url):
+#     df = pd.read_csv(url, index_col=0)
+#     df.insert(3, "radius", 0)
+#     df['radius'] = np.sqrt(df['    x-coordinate'] ** 2 + df['    y-coordinate'] ** 2)
+#     indexNames = df[(df['radius'] > 0.03)].index
+#     df.drop(indexNames, inplace=True)
+#     return df
+
+
+
+
+# df_1.plot(kind='scatter',x='radius',y=df_1_header_list[4],color='red')
+# plt.show()
