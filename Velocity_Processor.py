09637d2df5c38e5fafebae6eeda50ce21b07c4df
jtarriela <78770359+jtarriela@users.noreply.github.com>
2021-02-21 21:10:23 -0500

    Completed parsing of csv and plotting. document formatting of csv files. test features with other more df files

--- Velocity_Processor.py
+++ Velocity_Processor.py
@@ -31,11 +31,10 @@ class df_cleaner:
     #             indexNames = df[(df['radius'] > 0.03)].index
     #             df.drop(indexNames, inplace=True)
     #             self.df = df
-    def read_dataframe(self):
-        ### read dataframe
-        # for i in self.url_index:
-        #     df = pd.read_csv(self.url[i], index_col=0)
-
+    # def read_dataframe(self):
+    #     ### read dataframe
+    #     # for i in self.url_index:
+    #     #     df = pd.read_csv(self.url[i], index_col=0)
 
     def compute_radius(self):
         for i in self.url_index:
@@ -67,16 +66,55 @@ class df_cleaner:
         plt.show()
 
 ##class feature testing
+
 if __name__ == "__main__":
-    url_df_1 = r'C:\Users\Joseph Tarriela\Documents\GitHub\FDA_Blood_Pump\Velocity_Data\K-omega_6L_2500\VelocityMagnitude-Q-1-k-omg.csv'
-    url_df_2 = r'C:\Users\Joseph Tarriela\Documents\GitHub\FDA_Blood_Pump\Velocity_Data\K-omega_6L_2500\VelocityMagnitude-Q-1-k-omg.csv'
-    url_df_3 = r'C:\Users\Joseph Tarriela\Documents\GitHub\FDA_Blood_Pump\Velocity_Data\K-omega_6L_2500\VelocityMagnitude-Q-1-k-omg.csv'
+    url_df_1 = '/Users/jdtarriela/Documents/GitHub/FDA_Blood_Pump/Velocity_Data/K-omega_6L_2500/MeanVelocityMagnitude-Q-1-k-omg.csv'
+    url_df_2 = '/Users/jdtarriela/Documents/GitHub/FDA_Blood_Pump/Velocity_Data/K-omega_6L_2500/RMSEVelocityMagnitude-Q-1-k-omg.csv'
+    url_df_3 = '/Users/jdtarriela/Documents/GitHub/FDA_Blood_Pump/Velocity_Data/K-omega_6L_2500/VelocityMagnitude-Q-1-k-omg.csv'
 
     url_list = [url_df_1, url_df_2, url_df_3]
+    index = np.arange(0, len(url_list))
 
-    for csv in url_list:
-        df_obj = df_cleaner(csv)
-        df = df_obj.compute_radius()
+    master_df_list = [] # Empty dataframe list
+
+
+    for i in index:
+        df = pd.read_csv(url_list[i], index_col=0)
+        # master_df.append(df)
+
+        df.insert(3, "radius", 0)
+        df['radius'] = np.sqrt(df['    x-coordinate'] ** 2 + df['    y-coordinate'] ** 2)
+        indexNames = df[(df['radius'] > 0.03)].index
+        df.drop(indexNames, inplace=True)
+        df.drop([df.columns[0], df.columns[1], df.columns[2]], axis=1, inplace=True)
+
+        master_df_list.append(df) # append empty list with all dataframes
+
+    master_df = pd.DataFrame() # create empty dataframe
+    all_df = pd.concat(master_df_list, axis=1) # append empty dataframe with df from df list
+
+    colours = ['b', 'g', 'r', 'c', 'm', 'y', 'k', 'w']
+    df_column_names = []
+
+
+    for i in range(len(all_df.columns)):
+        if i%2 == 0 and i < len(all_df.columns)-1:
+            # print("even")
+            plt.plot(all_df.iloc[:, i], all_df.iloc[:, i + 1], colours[i],marker='o',linestyle='dashed', linewidth=1, markersize=5)
+            df_column_names.append(all_df.columns[i+1])
+        else:
+            # print("odd")
+            pass
+    #     plt.plot(all_df.iloc[:, i], all_df.iloc[:, i+1], colours[i])
+    plt.legend(df_column_names)
+    plt.ylabel('Velocity (m/s)')
+    plt.xlabel('Radius (m)')
+    plt.show()
+
+
+    # for csv in url_list:
+    #     df_obj = df_cleaner(csv)
+    #     df = df_obj.compute_radius()
         # df_plot = df_obj.velocity_plot()
 
         # df.drop([df.columns[0],df.columns[1],df.columns[2]], axis=1, inplace=True)
