b0846a44cd16a4724e04c935ad0f4ed57ddcec35
jtarriela <78770359+jtarriela@users.noreply.github.com>
2021-03-01 10:47:47 -0500

    update folder structure

--- Velocity_Data/K-omega_6L_2500/1_sample_per_50_timestep/K-omega_6L_2500_1_per_50.py
+++ Velocity_Data/K-omega_6L_2500/1_sample_per_50_timestep/K-omega_6L_2500_1_per_50.py
@@ -10,13 +10,13 @@ def plot_Q1(title,url_list):
 if __name__ == "__main__":
     # Quadrant 1 6L 2500 K-Omega
     Experimental_Q1 = 'https://raw.githubusercontent.com/jtarriela/FDA_Blood_Pump/main/Velocity_Data/Experimental_6L_2500/Q1_6L_2500.csv'
-    Q1_url_1 = 'https://raw.githubusercontent.com/jtarriela/FDA_Blood_Pump/main/Velocity_Data/K-omega_6L_2500/Quadrant%201%20Data/VelocityMagnitude-Q-1-k-omg.csv'
+    Q1_url_1 = 'https://raw.githubusercontent.com/jtarriela/FDA_Blood_Pump/main/Velocity_Data/K-omega_6L_2500/1_sample_per_50_timestep/Quadrant%201%20Data/MeanVelocityMagnitude-Q-1-k-omg.csv'
     # Q1_url_2 = 'https://raw.githubusercontent.com/jtarriela/FDA_Blood_Pump/main/Velocity_Data/K-omega_6L_2500/Quadrant%201%20Data/RMSEVelocityMagnitude-Q-1-k-omg.csv'
     # Q1_url_3 = 'https://raw.githubusercontent.com/jtarriela/FDA_Blood_Pump/main/Velocity_Data/K-omega_6L_2500/Quadrant%201%20Data/MeanVelocityMagnitude-Q-1-k-omg.csv'
 
     # Quadrant 2 6L 2500 K-Omega
     Experimental_Q2 = 'https://raw.githubusercontent.com/jtarriela/FDA_Blood_Pump/main/Velocity_Data/Experimental_6L_2500/Q2_6L_2500.csv'
-    Q2_url_1 = 'https://raw.githubusercontent.com/jtarriela/FDA_Blood_Pump/main/Velocity_Data/K-omega_6L_2500/Quadrant%202%20Data/VelocityMagnitude-Q-2-k-omg.csv'
+    Q2_url_1 = '___'
     # Q2_url_2 = 'https://raw.githubusercontent.com/jtarriela/FDA_Blood_Pump/main/Velocity_Data/K-omega_6L_2500/Quadrant%202%20Data/RMSEVelocityMagnitude-Q-2-k-omg.csv'
     # Q2_url_3 = 'https://raw.githubusercontent.com/jtarriela/FDA_Blood_Pump/main/Velocity_Data/K-omega_6L_2500/Quadrant%202%20Data/MeanVelocityMagnitude-Q-2-k-omg.csv'
 
@@ -29,5 +29,5 @@ if __name__ == "__main__":
     Q1_plot = plot_Q1(Q1_title, Q1_url_list)
 
     # Quadrant 2 Plot
-    Q2_title = "Quadrant 2: 6L 2500RPM K-Omg"
-    Q2_plot = plot_Q1(Q2_title, Q2_url_list)
+    # Q2_title = "Quadrant 2: 6L 2500RPM K-Omg"
+    # Q2_plot = plot_Q1(Q2_title, Q2_url_list)
