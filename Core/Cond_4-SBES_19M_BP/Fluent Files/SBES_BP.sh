15df4f52c934d161398eed8bd8c61686b34e434e
jtarriela <78770359+jtarriela@users.noreply.github.com>
2021-08-05 02:57:53 -0400

    Journal File Updates

--- Core/Cond_4-SBES_19M_BP/Fluent Files/SBES_BP.sh
+++ Core/Cond_4-SBES_19M_BP/Fluent Files/SBES_BP.sh
@@ -4,13 +4,13 @@
 #SBATCH --qos=bfbsm19
 #SBATCH --partition=bfbsm_2019
 
-#SBATCH --nodes=3
+#SBATCH --nodes=5
 #SBATCH --sockets-per-node=2
 #SBATCH --ntasks-per-node=24
-#SBATCH --mem-per-cpu=7000
-#SBATCH --job-name=Cond_1-SBES_24M_BP
+#SBATCH --mem-per-cpu=7500
+#SBATCH --job-name=C4_19
 #SBATCH --output=CY_artery
-#SBATCH --time=1000:00:00
+#SBATCH --time=355:00:00
 
 
 #### SLURM 2 node, 24 processor per node Ansys Fluent test to run for 30min.
@@ -21,4 +21,4 @@ module add apps/ansys/2020r1
 # Create our hosts file ala slurm
 srun hostname -s |sort -V > $(pwd)/slurmhosts.$SLURM_JOB_ID.txt
 
-time fluent 3ddp -g -t${SLURM_NTASKS} -slurm -pib.infinipath -mpi=ibmmpi -cnf=$(pwd)/slurmhosts.$SLURM_JOB_ID.txt -i SBES_SS.jou
+time fluent 3ddp -g -t${SLURM_NTASKS} -slurm -pib.infinipath -mpi=ibmmpi -cnf=$(pwd)/slurmhosts.$SLURM_JOB_ID.txt -i C4_SS_SBES_19M.jou
