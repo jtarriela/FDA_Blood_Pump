d0c726ea56e7867fb0101f76836b1f0d6d578408
jtarriela <jtarriela@usf.edu>
2021-09-22 14:59:42 -0400

    C1 C4 SBES DATA

--- postpro/Concatenate_del_duplicates_residual.py
+++ postpro/Concatenate_del_duplicates_residual.py
@@ -3,23 +3,44 @@ import os
 import shutil
 import hashlib
 
-# print(glob.glob(r"/Users/jdtarriela/Desktop/C5_Residuals/0-6400/*.out"))
-dir_list = [r'/Users/jdtarriela/Desktop/C5_Residuals/0-6400/',
-            r'/Users/jdtarriela/Desktop/C5_Residuals/6400-8000/',
-            r'/Users/jdtarriela/Desktop/C5_Residuals/15000-22800/',
-            r'/Users/jdtarriela/Desktop/C5_Residuals/22800-24000/']
+# todo: get files to use
+filenames = [os.path.basename(x) for x in glob.glob(r"/Users/jdtarriela/Downloads/C1/20400-24000/*.out")]
 
-#todo: get files to use
-#file_list = glob.glob(r"/Users/jdtarriela/Desktop/C5_Residuals/0-6400/*.out")
-filenames = [os.path.basename(x) for x in glob.glob(r"/Users/jdtarriela/Desktop/C5_Residuals/0-6400/*.out")]
+dir_list_1 = [r'/Users/jdtarriela/Downloads/C1/20400-24000/',
+            r'/Users/jdtarriela/Downloads/C1/13800-20400/',
+            r'/Users/jdtarriela/Downloads/C1/6800-13800/',
+            r'/Users/jdtarriela/Downloads/C1/6200-6800/',
+            r'/Users/jdtarriela/Downloads/C1/0-6200/']
+
+path_input = r'/Users/jdtarriela/Downloads/C1/temp_files/'
+path_output = r'/Users/jdtarriela/Downloads/C1/Final_residuals/'
+
+# Check whether the
+# specified path is
+# an existing file
+IO_path = [path_input, path_output]
+for i in IO_path:
+    isFile = os.path.isfile(i)
+    if isFile == True:
+        print('Path {} exists'.format(i))
+        print('Script Break, remove temp folder')
+        break
+    else:
+        print('Path {} does not exist'.format(i))
+        os.mkdir(i)
+        print('Path {} created \n'.format(i))
 
 #todo: append files by searching eahc folder save to ~/appended
 for file in filenames:
     # Reset Directory list to be appended for every file
-    dir_list = [r'/Users/jdtarriela/Desktop/C5_Residuals/0-6400/',
-                r'/Users/jdtarriela/Desktop/C5_Residuals/6400-8000/',
-                r'/Users/jdtarriela/Desktop/C5_Residuals/15000-22800/',
-                r'/Users/jdtarriela/Desktop/C5_Residuals/22800-24000/']
+    # Function doesnt like when dir_list outside for list
+
+    # dir_list = [r'/Users/jdtarriela/Desktop/C5_Residuals/0-6400/',
+    #             r'/Users/jdtarriela/Desktop/C5_Residuals/6400-8000/',
+    #             r'/Users/jdtarriela/Desktop/C5_Residuals/15000-22800/',
+    #             r'/Users/jdtarriela/Desktop/C5_Residuals/22800-24000/']
+
+    dir_list = dir_list_1[:] # [:] copy explicity without reference
     # Append directories with file name
     for i in enumerate(dir_list):
         root = dir_list[i[0]]
@@ -28,7 +49,7 @@ for file in filenames:
         # print(i)
 
     # Concatenate text files together and save out to new folder
-    output_path = r'/Users/jdtarriela/Desktop/C5_Residuals/appended/' + file
+    output_path = path_input + file
     with open(output_path, 'wb') as wfd:
         for f in dir_list:
             with open(f, 'rb') as fd:
@@ -36,8 +57,6 @@ for file in filenames:
 
 #todo: remove duplicates for i in filenames
 #todo: fix overwriting leaving empty files
-path_input = r'/Users/jdtarriela/Desktop/C5_Residuals/appended/'
-path_output = r'/Users/jdtarriela/Desktop/C5_Residuals/del_duplicates/'
 for file in filenames:
     print(file)
     print(path_output)
@@ -69,4 +88,4 @@ for file in filenames:
             if pos != 0 and pos != 2:
                 outfile.write(line)
 
-
+shutil.rmtree(path_input, ignore_errors = False)
